9a8991b1fe6f1fac60a27d592afcaf755609ae37
aserelnaghy <aserelnaghy01@gmail.com>
2025-10-19 17:57:39 +0300

    configurations set up

---
+++ config.py
@@ -0,0 +1,43 @@
+"""
+config.py
+Global configuration settings for the Personal Finance Manager project.
+All paths and constants defined here are imported across modules.
+"""
+
+from pathlib import Path
+import os
+
+# -------------------------------
+# Environment and base paths
+# -------------------------------
+
+# Root directory (auto-detect project root)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATA_DIR = BASE_DIR / 'data'
+BACKUP_DIR = DATA_DIR / 'backups'
+
+#File Names
+USERS_FILE = DATA_DIR / 'users.json'
+TRANSACTIONS_FILE = DATA_DIR / 'transactions.json'
+
+#Ensure data directories exist
+os.makedirs(DATA_DIR, exist_ok=True) # Create data directory if it doesn't exist
+os.makedirs(BACKUP_DIR, exist_ok=True) # Create backup directory if it doesn't exist
+
+# -------------------------------
+# Application settings
+# -------------------------------
+APP_NAME = "Personal Finance Manager"
+APP_VERSION = "1.0"
+DEFAULT_CURRENCY = "EGP"
+DATE_FORMAT = "%Y-%m-%d"  # ISO standard
+AUTO_BACKUP_LIMIT = 5     # keep last 5 backups
+
+# -------------------------------
+# Feature flags
+# -------------------------------
+
+ENABLE_RECURRING = True
+ENABLE_BUDGETS = True
+ENABLE_GOALS = True
