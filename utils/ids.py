8a07df9f1ebc0d91196317a0a4544bdfdcea5966
aserelnaghy <aserelnaghy01@gmail.com>
2025-10-20 19:38:25 +0300

    id functions edited

--- utils/ids.py
+++ utils/ids.py
@@ -1,12 +1,17 @@
 import uuid
 import time
 
-def generate_unique_id() -> str:
+def generate_user_id() -> str:
     """ Generate a unique ID for users. """
-    return str(uuid.uuid4())
+    short_uuid = uuid.uuid4().hex[:6]
+    return f"USR-{short_uuid}"
 
 def generate_transaction_id() -> str:
     """Generate a unique ID for transactions (TXN-<timestamp>-<short-uuid>)."""
     timestamp = int(time.time())
-    short_uuid = uuid.uuid4().hex[:6]
-    return f"TXN-{timestamp}-{short_uuid}"
+    short_uuid = (uuid.uuid4().hex[:6])
+    return f"TXN-{short_uuid}-{timestamp}"
+
+# if __name__ == "__main__":
+    # print("Unique User ID:", generate_user_id())
+    # print("Unique Transaction ID:", generate_transaction_id())
