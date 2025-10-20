e3f74b8600a2d0b5044e5b05990f489107595d44
aserelnaghy <aserelnaghy01@gmail.com>
2025-10-20 20:16:28 +0300

    custom errors defined

--- utils/errors.py
+++ utils/errors.py
@@ -1,1 +1,19 @@
-# errors.py
+class UserNotFoundError(Exception):
+    """Raised when a user cannot be found in the database."""
+    pass
+
+class InvalidTransactionError(Exception):
+    """Raised when a transaction has invalid or missing data."""
+    pass
+
+class DataPersistenceError(Exception):
+    """Raised when saving or loading data fails."""
+    pass
+
+class AuthenticationError(Exception):
+    """Raised when login or password verification fails."""
+    pass
+
+class InvalidDateError(Exception):
+    """Raised when a date is in an incorrect format or out of range."""
+    pass
